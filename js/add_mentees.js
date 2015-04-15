e15c215cc45930a444b55f75a60cfe373a60aea5
Ben Shryock <benshry@gmail.com>
2015-04-15 00:13:23 -0400

    local storage for add_mentee page

--- js/add_mentees.js
+++ js/add_mentees.js
@@ -6,16 +6,28 @@ $(document).ready(function() {
   var load = function() {
 
     if (typeof(Storage) !== "undefined") {
+
       for (var i in localStorage) {
         // remove mentees that already appear in localStorage from list of add-able mentees
+        $(".mentee[data-id=" + i + "]").remove()
       }
+
     }
     else {
       console.log("No Local Storage");
     }
   }
 
-  // add event handler for adding new mentees to localStorage
+  // Event handler for adding new mentees to localStorage
+  $(".check_mentee").change(function() {
+    var mentee_id = $(this).closest('.mentee').attr("data-id");
+    if (this.checked) {
+      localStorage.setItem(mentee_id, mentee_id);
+    }
+    else {
+      localStorage.removeItem(mentee_id);
+    }
+  });
 
   // Load locally stored mentees and use them to update the page
   load();
