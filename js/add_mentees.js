d73a7f971442b95213d53cd9b1e85db045910e57
Ben Shryock <benshry@gmail.com>
2015-04-16 03:28:10 -0400

    new mentees format in local storage

--- js/add_mentees.js
+++ js/add_mentees.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
       for (var i in localStorage) {
         // remove mentees that already appear in localStorage from list of add-able mentees
         $(".mentee[data-id=" + i + "]").remove()
-      }
 
+      }
     }
     else {
       console.log("No Local Storage");
@@ -21,8 +21,10 @@ $(document).ready(function() {
   // Event handler for adding new mentees to localStorage
   $(".check_mentee").change(function() {
     var mentee_id = $(this).closest('.mentee').attr("data-id");
+    var mentee_name = $(this).closest('.mentee').attr("data-name");
     if (this.checked) {
-      localStorage.setItem(mentee_id, mentee_id);
+      var mentee = {id:mentee_id, name:mentee_name, notes:[], events:[]}
+      localStorage.setItem(mentee_id, JSON.stringify(mentee));
     }
     else {
       localStorage.removeItem(mentee_id);
