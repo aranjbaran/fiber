b1af91de81d336d5dbc222d693d66a2a1c059390
ehoulihan <houlihan.e.j@gmail.com>
2015-04-16 14:36:07 -0400

    adding and saving notes v1.0

---
+++ js/notes.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+  // Initialize namespace
+  Mentees = {};
+
+  var load = function() {
+
+    if (typeof(Storage) !== "undefined") {
+
+      // get id of current page
+      var id = $("#content-main").attr("data-id");
+      console.log(id)
+
+      // index into local storage using id of current page
+      var notes = JSON.parse(localStorage[id])["notes"]
+
+      // iterate through notes and display on page
+      for (var i = 0; i < notes.length ; i++){
+        $("#notes").append("<div>"+ notes[i] +"</div>")
+      }
+      
+    }
+    else {
+      console.log("No Local Storage");
+    }
+  }
+
+  // Load locally stored mentees and use them to update the page
+  load();
+
+});
+
+// saves a new note to local storage and adds the note
+function add_note() {
+  // add new notes
+  var note = prompt("Enter Your Note", "Today...");
+  $("#notes").append("<div>"+ note +"</div>")
+  // save new note
+  if (typeof(Storage) !== "undefined") {
+    var id = $("#content-main").attr("data-id")
+    var profile = JSON.parse(localStorage[id])
+    profile["notes"].push(note)
+    localStorage[id] = JSON.stringify(profile)
+  }
+} 
+        
