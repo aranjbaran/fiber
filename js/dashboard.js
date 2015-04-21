c28e5d2ffd18cecde77b6cf4e5da879896fa4756
aranjbaran <aranjbaran@college.harvard.edu>
2015-04-21 16:41:51 -0400

    changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -11,9 +11,26 @@ $(document).ready(function() {
       $("#num-mentees").html(localStorage.length);
 
       for (var i in localStorage) {
-        // Only show mentees if they appear in localStorage
+        // Only show mentees in sidebar if they appear in localStorage
         $(".contact[data-id=" + i + "]").show();
+
+        // Load upcoming events and add to page
+        var calendar_events = [];
+        var events = JSON.parse(localStorage[i])["events"];
+        for (i in events) {
+          res = events[i].split('__');
+          calendar_events.push({title:res[1], start:res[0]});
+          // $("#upcoming").append("<div class='event'>" + events[i] + "</div>");
+        }
       }
+
+      $('#calendar').fullCalendar({
+        events: calendar_events,
+        height: 150,
+        defaultView: "basicWeek",
+        weekends: false,
+        columnFormat: "ddd"
+      })
     }
     else {
       console.log("No Local Storage");
